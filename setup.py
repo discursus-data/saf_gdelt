4b1b3c09de956a3133a6cb7974a6e070f2dbf093
Olivier Dupuis <odupuis@lantrns.co>
2022-06-28 12:16:02 -0400

    Trying to point to library script to run shell on

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='discursus_gdelt',
     packages=find_packages(include=['discursus_gdelt']),
+    scripts=[
+        'discursus_gdelt/miners/gdelt_events_miner.zsh', 
+        'discursus_gdelt/miners/gdelt_mentions_miner.zsh'
+    ],
     version='0.1.0',
     description='GDELT mining package for the discursus platform',
     author='Olivier Dupuis',
